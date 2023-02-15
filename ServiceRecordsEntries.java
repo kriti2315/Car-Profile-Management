05e1bfc4a60852fa38480662419c3453d545e2ae
kriti2315 <63349606+kriti2315@users.noreply.github.com>
2023-02-15 17:21:42 +0530

    Add files via upload

---
+++ ServiceRecordsEntries.java
@@ -0,0 +1,15 @@
+public class ServiceRecordsEntries {
+    //enter service records
+    int ServiceNo;
+    String ServiceDate;
+    int ServiceCost;
+    int Mileage;
+
+    ServiceRecordsEntries(int ServiceNo,String ServiceDate,int ServiceCost , int Mileage){
+        this.ServiceNo = ServiceNo;
+        this.ServiceDate = ServiceDate;
+        this.ServiceCost = ServiceCost;
+        this.Mileage = Mileage;
+
+    }
+}
